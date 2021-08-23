bea293d06bd8ee8d169631b3425b4ef0f578f6d0
D Vignesh Kumar <41253273+legendvi@users.noreply.github.com>
2021-08-23 15:17:13 +0530

    Update script.js
    added more form input and table elements

--- script.js
+++ script.js
@@ -12,20 +12,42 @@ const tbody = document.querySelector("#tbody");
 const firstname = document.querySelector("#FirstName");
 const lastname = document.querySelector("#LastName");
 const email = document.querySelector("#Email");
+const gender = document.querySelector("#Gender");
+const food = document.querySelectorAll(".Checkbox");
+const pincode = document.querySelector("#Pincode");
+const address = document.querySelector("#Address");
+const state = document.querySelector("#State");
+const country = document.querySelector("#Country");
 //Array to store all objects created
 let rowArray = [];
 //state and element where we have to Edit
 let editing = false;
 let editElement;
+console.log(gender);
 //----------------------------------------------------------------------------------------------------
-
 //Class table to store the row data in object---------------------------------------------------------
 class Table {
-  constructor(firstname, lastname, email) {
+  constructor(
+    firstname,
+    lastname,
+    email,
+    address,
+    pincode,
+    gender,
+    checkedFood,
+    state,
+    country
+  ) {
     this.firstname = firstname;
     this.lastname = lastname;
     this.email = email;
     this.id = this.count;
+    this.address = address;
+    this.pincode = pincode;
+    this.gender = gender;
+    this.checkedFood = checkedFood;
+    this.state = state;
+    this.country = country;
   }
 }
 //----------------------------------------------------------------------------------------------------
@@ -43,6 +65,12 @@ function updateTable() {
   <td>
    ${curElement.email}
    </td>
+   <td>${curElement.address}</td>
+   <td>${curElement.pincode}</td>
+   <td>${curElement.gender}</td>
+   <td>${curElement.checkedFood}</td>
+   <td>${curElement.state}</td>
+   <td>${curElement.country}</td>
    <td>
     <button class="btn btn-secondary ms-3 edit btn-sm" >
       Edit
@@ -60,10 +88,16 @@ function updateTable() {
   setLocal();
 }
 //Element that changes after clicking edit
-function changeElement(element) {
+function changeElement(element, checkedFood) {
   element.firstname = firstname.value;
   element.lastname = lastname.value;
   element.email = email.value;
+  element.address = address.value;
+  element.pincode = pincode.value;
+  element.gender = gender.value;
+  element.checkedFood = checkedFood;
+  element.state = state.value;
+  element.country = country.value;
   editing = false;
   //----------------------------------------------------------------------------------------------------
 }
@@ -72,12 +106,42 @@ function changeElement(element) {
 //create new element or call function to update exsiting element
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+  //Array to store choice of food
+  const checkedFood = [];
+  food.forEach(function (el) {
+    if (el.checked == true) {
+      checkedFood.push(el.value);
+    }
+  });
+  if (checkedFood.length < 2) {
+    alert("Select at least two food items");
+    return;
+  }
+  console.log(checkedFood);
   if (!editing) {
-    const row = new Table(firstname.value, lastname.value, email.value);
+    const row = new Table(
+      firstname.value,
+      lastname.value,
+      email.value,
+      address.value,
+      pincode.value,
+      gender.value,
+      checkedFood,
+      state.value,
+      country.value
+    );
     rowArray.push(row);
-  } else changeElement(editElement);
-  firstname.value = lastname.value = email.value = "";
-
+  } else changeElement(editElement, checkedFood);
+  firstname.value =
+    lastname.value =
+    email.value =
+    address.value =
+    pincode.value =
+    state.value =
+    country.value =
+      "";
+  gender.value = "";
+  food.forEach((el) => (el.checked = false));
   updateTable();
 });
 //Listen to click event in table and look for target delete to delete selected row
@@ -101,12 +165,22 @@ table.addEventListener("click", function (e) {
     firstname.value = editElement.firstname;
     lastname.value = editElement.lastname;
     email.value = editElement.email;
+    address.value = editElement.address;
+    pincode.value = editElement.pincode;
+    gender.value = editElement.gender;
+    state.value = editElement.state;
+    country.value = editElement.country;
+    food.forEach((element) => {
+      if (editElement.checkedFood.includes(element.value)) {
+        element.checked = true;
+      }
+    });
     editing = true;
   }
 });
 //-----------------------------------------------------------------------------------------------------
-//Setting Retrieving and deleting Local storage-------------------------------------------------------
 
+//Setting Retrieving and deleting Local storage-------------------------------------------------------
 //set local Storage
 function setLocal() {
   localStorage.setItem("table", JSON.stringify(rowArray));
@@ -122,6 +196,6 @@ function reset() {
   localStorage.removeItem("table");
   location.reload();
 }
-//rest add event Listener
+//add event Listener to reset button
 btn_reset.addEventListener("click", reset);
 //----------------------------------------------------------------------------------------------------
